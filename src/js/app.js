3b286e826db9a1de49f36481ca5404ba24283fa1
Vadim <vadimkozak02@icloud.com>
2023-05-10 22:29:31 +0300

    fixed bags

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,23 @@
 new Swiper('.slider', {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
 });
+
+// burger menu
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header-navigation');
+    const menuClose = document.querySelector('.header-nav__close');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header-navigation-active');
+    });
+    menuClose.addEventListener('click', () => {
+        menu.classList.remove('header-navigation-active');
+    });
+}());
